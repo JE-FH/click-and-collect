5ea3868c68b145e90c1113e78491f582aa4724d2
Karl Barnholdt Kragsaa <karlbarnholdt@gmail.com>
2021-03-26 15:11:10 +0100

    Created a template for the server

---
+++ backend/server.js
@@ -0,0 +1,59 @@
+import http from 'http';
+
+const port = 8000;
+const hostname = '127.0.0.1';
+
+const server = http.createServer(requestHandler);
+
+function requestHandler(request, response) {
+    console.log("Received " + request.method + " " + request.url);
+
+    switch(request.method) {
+        case "POST": {
+            switch(request.url) {
+                default:
+                    defaultResponse(response);
+                    break;
+            }
+            break;
+        }
+        case "GET": {
+            switch(request.url) {
+                case "/api/add_package":
+                    add_package();
+                    break;
+                default:
+                    defaultResponse(response);
+                    break;
+            }
+            break;
+        }
+        default:
+            defaultResponse(response);
+            break;
+    }
+}
+
+/* Request handler for any endpoint that isn't explicitally handled */
+function defaultResponse(response) {
+    console.log("Nothing is here");
+    response.setHeader('Content-Type', 'text/plain');
+    response.write(' ');
+    response.end("\n");
+    response.statusCode = 404;
+}
+
+/* Example of a HTTP request case */
+function add_package() {
+    console.log('No API');
+    response.setHeader('Content-Type', 'text/plain');
+    response.write(' ');
+    response.end("\n");
+    response.statusCode = 404;
+}
+
+
+/* Starts the server */
+server.listen(port, hostname, () => {
+    console.log("Server listening on " + hostname + ":" + port);
+})
