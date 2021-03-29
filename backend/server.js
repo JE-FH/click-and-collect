11db158b0d9f4244d3d396bf53262cc57cb2f650
Papzi <gamerboy53@live.dk>
2021-03-29 15:25:36 +0200

    created cookie middleware

--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,18 @@
 const http = require("http");
 const sqlite3 = require("sqlite3");
 const fs = require("fs/promises");
+const crypto = require("crypto");
+const cookie = require('cookie');
 
 const port = 8000;
 const hostname = '127.0.0.1';
 
 let db;
 
-function requestHandler(request, response) {
+async function requestHandler(request, response) {
     console.log("Received " + request.method + " " + request.url);
+    
+    cookieMiddleware(request, response);
 
     switch(request.method) {
         case "POST": {
@@ -52,6 +56,49 @@ function add_package() {
     response.write(' ');
     response.end("\n");
     response.statusCode = 404;
+}
+
+/* cant contain ( ) < > @ , ; : \ " / [ ] ? = { } per https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Set-Cookie */
+const COOKIES_SESSION_ID = "sessid";
+/* Seconds until the cookie expires */
+const COOKIES_EXPIRE_TIME = 60*60*24; /*24 hours*/
+/* This is where all the session data is stored */
+let sessionStore = new Map();
+
+function cookieMiddleware(req, res) {
+    /* Check if the user has any cookies set in the header */
+    if (typeof(req.headers["cookie"]) == "string") {
+        let cookies = cookie.parse(req.headers["cookie"]);
+        /* Check if the user has the specific cookie where we store session id */
+        if (typeof(cookies[COOKIES_SESSION_ID]) == "string") {
+            /* Check if that session exists in our session store */
+            let store = sessionStore.get(cookies[COOKIES_SESSION_ID]);
+            if (store != null) {
+                /* Check if the cookie has expired, getTime() returns time in milliseconds*/
+                if (new Date().getTime() > store.create_time.getTime() + COOKIES_EXPIRE_TIME * 1000) {
+                    sessionStore.delete(cookies[COOKIES_SESSION_ID]);
+                } else {
+                    req.session = store.data;
+                    return;
+                }
+            }
+        }
+    }
+    /* Client doesnt have a valid session id, so we create one */
+    let id = crypto.randomBytes(16).toString("hex");
+
+    if (sessionStore.has(id)) {
+        /* session id is already used which has around 1 in 3e+38 chance of happening */
+        throw new Error("Super duper unlucky, lets just throw an error");
+    }
+
+    /* We add a create time so that we can expire old sessions */
+    sessionStore.set(id, {create_time: new Date(), data: {}});
+
+    req.session = sessionStore.get(id).data;
+
+    /* Set the session cookie */
+    res.setHeader("Set-Cookie", `${COOKIES_SESSION_ID}=${id}`);
 }
 
 
