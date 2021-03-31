da14a6e160181658b2bcebd98c60226e8ff9222c
BooTheGhoster <73188508+BooTheGhoster@users.noreply.github.com>
2021-03-31 14:57:03 +0200

    Opdateret med tilføj medarbejder og størstedelen af remove

--- backend/server.js
+++ backend/server.js
@@ -26,6 +26,13 @@ async function requestHandler(request, response) {
                 case "/login":
                     login_post(request, response);
                     break;
+                case "/admin/employees/add":
+                    add_employee_post(request, response);
+                    break;
+                case "/admin/employees/remove":
+                    remove_employee_post(request,response);
+                break;
+
             }
             break;
         }
@@ -41,8 +48,11 @@ async function requestHandler(request, response) {
                     admin_dashboard(request, response);
                     break;
                 case "/admin/employees/add":
-                    add_employee(request, response);
+                    add_employee(request, response, "");
                     break;
+                case "/admin/employees/remove":
+                    remove_employee(request,response, "");
+                break;
                 default:
                     defaultResponse(response);
                     break;
@@ -101,8 +111,9 @@ function cookieMiddleware(req, res) {
             }
         }
     }
+
     /* Client doesnt have a valid session id, so we create one */
-    let id = crypto.randomBytes(16).toString("hex");
+    let id = crypto.randomBytes(16).toString(HASHING_HASH_ENCODING);
 
     if (sessionStore.has(id)) {
         /* session id is already used which has around 1 in 3e+38 chance of happening */
@@ -239,8 +250,10 @@ async function login_post(request, response) {
 
         if (user.superuser) {
             response.setHeader('Location','/admin');
+            response.end();
         } else {
             response.setHeader('Location','/store');
+            response.end();
         }
         response.end();
 
@@ -303,11 +316,306 @@ async function main() {
 
 
 function admin_dashboard(request, response){
-    console.log(request.user);
-    console.log(request.session);
+    if (request.user === null || request.user.superuser == 0){
+        adminNoAccess(request, response);
+    } 
+    else {
+        response.statusCode = 302;
+        response.write(`
+    <!DOCTYPE html>
+    <html>
+        <head>
+            <title>Dashboard</title>
+        </head>
+
+        <body>
+            <a href="/admin/employees/add"> Add new employee </a> <br>
+            <a href="/admin/employees/remove"> Remove employee </a>
+        </body>
+    </html>
+    `);
+        response.end();
+    }
+
 }
-function add_employee(request, response){
+
+function adminNoAccess(request, response){
+    response.statusCode = 401;
+    response.write(`
+    <!DOCTYPE html>
+    <html>
+        <head>
+            <title>You are not logged in</title>
+        </head>
+
+        <body>
+            You need to be logged in as store admin to access this site.
+            <br>
+            <a href="/login"> Go to login site</a>
+        </body>
+    </html>
+    `);
+    response.end();
+}
+
+function add_employee(request, response, error){
+    if (request.user === null || request.user.superuser == 0){
+        adminNoAccess(request, response);
+    }
+    else{
+        response.statusCode = 200;
+        response.write(`
+        <!DOCTYPE html>
+        <html>
+            <head>
+                <title>Adding new employee </title>
+            </head>
+            <body>
+                ${error ? `<p>${error}</p>` : ""}
+                <a href="/admin"> Go to admin startpage </a> <br>
+                <h> Adding new employee to the store <h>
+                
+                <form action="/admin/employees/add" method="POST">
+                <label for="username">Username:      </label>
+                <input type="text" name="username" placeholder="username" required><br>
+
+                <label for="name"> Employee name: </label>
+                <input type="text" name="employee_name" placeholder="Employee name" required><br> <br>
+
+                <label for="password"> Password:     </label>
+                <input type="password" name="password" placeholder="password" id="password" onchange='check_pass();' required> <br>
+
+                <label for="confirm_password"> Confirm password: </label>
+                <input type="password" name="confirm_password" placeholder="password" id="confirm_password" onchange='check_pass();' required> <br>
+
+                <p id="matching_passwords" style="color:red" hidden> The passwords do not match </p>
+                
 
 
+
+                <label for="superuser"> Is the account an admin account: </label>
+                <div id="wrapper">
+    
+                <p>
+                <input type="radio" name="superuser" checked>Yes</input>
+                </p>
+                <p>
+                <input type="radio" name="superuser">No</input>
+                </p>
+                </div>
+                <br>
+            
+
+                <input type="submit" id="submit" value="Create user" disabled>
+            </form>
+            <script>
+            function check_pass() {
+                if (document.getElementById('password').value ==
+                        document.getElementById('confirm_password').value) {
+                    document.getElementById('submit').disabled = false;
+                    document.getElementById('matching_passwords').hidden = true;
+                } else {
+                    document.getElementById('submit').disabled = true;
+                    document.getElementById('matching_passwords').hidden = false;
+                    
+                }
+            }
+            </script>
+            </body>
+        </html>
+        `);
+        response.end();
+    }
+    
+
+}
+async function add_employee_post(request, response){
+    // if (request.user === null || request.user.superuser == 0){
+    //     adminNoAccess(request, response);
+    // }
+    // else{
+        let post_body = await new Promise((resolve, reject) => {
+            let body = ''
+            request.on('data', function(data) {
+              body += data;
+            })
+            request.on('end', function() {
+              resolve(body);
+            })
+        });
+        console.log('Body: ' + post_body);
+    
+        let post_parameters = {};
+    
+        /* Decode the key value pairs from the url encoding */
+        post_body.split("&").map((v) => {
+            let split = v.split("=");
+            post_parameters[decodeURIComponent(split[0])] = decodeURIComponent(split[1]);
+        });
+    
+        /*  Dårlig måde aat håndtere fejl*/
+        if (!(typeof post_parameters["username"] == "string" && typeof post_parameters["password"] == "string" && typeof post_parameters["employee_name"] == "string")) { 
+            response.statusCode = 400;
+            response.write("Some of the input is wrong");
+            response.end();
+            return;
+        }
+    
+        /* Find the user if it exists */
+        let username_unique = await new Promise((resolve, reject) => {
+            db.serialize(() => {
+                db.get("SELECT id FROM user WHERE username=?", [post_parameters["username"]], (err, row) => {
+                    if (err) {
+                        resolve(null);
+                    } else {
+                        if (row == undefined) {
+                            resolve(true);
+                        } else {
+                            add_employee(request, response, "Username already exists")
+                            resolve(false);
+                        }
+                    }
+                })
+            });
+        });
+    
+        let employee_name_unique = await new Promise((resolve, reject) => {
+            db.serialize(() => {
+                db.get("SELECT id FROM user WHERE name=?", [post_parameters["employee_name"]], (err, row) => {
+                    if (err) {
+                        resolve(null);
+                    } else {
+                        if (row == undefined) {
+                            resolve(true);
+                        } else {
+                            add_employee(request, response, "User with employee name already exists");
+                            resolve(false);
+                            return;
+                        }
+                    }
+                })
+            });
+        });
+        console.log(post_parameters["password"]);
+        if (username_unique && employee_name_unique) {
+            let salt = crypto.randomBytes(16).toString(HASHING_HASH_ENCODING);
+            let hashed = await new Promise((resolve, reject) => {
+                crypto.pbkdf2(post_parameters["password"], salt, HASHING_ITERATIONS, HASHING_KEYLEN, HASHING_ALGO, (err, derivedKey) => {
+                    if (err) {
+                        reject(err);
+                    }
+                    resolve(derivedKey);
+                });
+            });
+            
+            db.run("INSERT INTO user (name, username, superuser, storeid, password, salt) VALUES (?, ?, ?, ?, ?, ?)", [[post_parameters["employee_name"]],[post_parameters["username"]], [post_parameters["superuser"]] == "on" ? true : false, request.user.storeId, hashed.toString(HASHING_HASH_ENCODING), salt]);
+            }
+            console.log("Bruger indsat i databasen");
+            add_employee(request, response, "User succesfully added to database");
+    }
+// }
+
+function remove_employee(request,response, error){
+    if (request.user === null || request.user.superuser == 0){
+        adminNoAccess(request, response);
+    }
+    else{
+        response.statusCode = 200;
+        
+        let sql = "SELECT DISTINCT Username username FROM user ORDER BY username";
+        
+        i = 0;
+        a = [0,2];
+        console.log(typeof(a));
+        console.log(a.Length);
+        db.all(sql, [], (err, rows) => {
+            if (err) {
+                throw err;
+            }
+            rows.forEach((row) => {
+                b = row.username;
+                a[i] = b;
+            console.log(b);
+                i++;
+            });
+        });
+        
+        console.log(a);
+        console.log(a.Length);
+    
+     
+        response.write(`
+        <!DOCTYPE html>
+        <html>
+            <head>
+                <title>Removing an employee </title>
+            </head>
+            <body>
+                ${error ? `<p>${error}</p>` : ""}
+                <a href="/admin"> Go to admin startpage </a> <br>
+                <h> Removing employee from the store <h>
+                
+                <form action="/admin/employees/remove" method="POST">
+                
+                <label for="name">Write the username:     </label>
+                <input type="text" placeholder="username" name="username" required><br>               
+
+                <input type="submit" value="Delete user" onclick="return confirm('Are you sure?')" />
+            </form>
+            
+            </body>
+        </html>
+        `);
+        response.end();
+        }
+}
+
+async function remove_employee_post(request, response){
+    if (request.user === null || request.user.superuser == 0){
+        adminNoAccess(request, response);
+    }
+    else{
+        let post_body = await new Promise((resolve, reject) => {
+            let body = ''
+            request.on('data', function(data) {
+              body += data;
+            })
+            request.on('end', function() {
+              resolve(body);
+            })
+        });
+        console.log('Body: ' + post_body);
+        
+        let post_parameters = {};
+
+        post_body.split("&").map((v) => {
+            let split = v.split("=");
+            post_parameters[decodeURIComponent(split[0])] = decodeURIComponent(split[1]);
+        });
+        let user = await new Promise((resolve, reject) => {
+            db.serialize(() => {
+                db.get("SELECT id, password, salt, storeId, superuser FROM user WHERE username=?", [post_parameters["username"]], (err, row) => {
+                    if (err) {
+                        resolve(null);
+                    } else {
+                        if (row == undefined) {
+                            resolve(null);
+                        } else {
+                            resolve(row);
+                        }
+                    }
+                })
+            });
+        });
+        if (user == null){
+            error = "Bruger ikke fundet";
+        }
+        else{
+            error = "Bruger slettet";
+        }
+        db.run("DELETE FROM user WHERE username=?", [post_parameters["username"]]);
+        console.log(user);
+        remove_employee(request, response, error);
+    }
 }
 main();
