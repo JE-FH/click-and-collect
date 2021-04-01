3a86e867faa237a439c7b027b9d734aee2381c93
Papzi <gamerboy53@live.dk>
2021-04-01 12:25:33 +0200

    added exec

--- backend/db-helpers.js
+++ backend/db-helpers.js
@@ -17,7 +17,7 @@ exports.db_all = async function db_all(db, query, param) {
 			 	resolve(rows);
 			}
 		});
-	}) 
+	});
 }
 
 /**
@@ -35,8 +35,8 @@ exports.db_get = async function db_get(db, query, param) {
 			} else {
 				resolve(row);
 			}
-		})
-	})
+		});
+	});
 }
 
 /**
@@ -53,6 +53,24 @@ exports.db_run = async function db_run(db, query, param) {
 			} else {
 				resolve();
 			}
-		})
-	})
+		});
+	});
+}
+
+/**
+ * 
+ * @param {sqlite3.Database} db the database to target
+ * @param {string} query the sql query to execute
+ * @param {any[]} param the parameters to replace ? with
+ */
+exports.db_exec = async function db_exec(db, query, param) {
+	return await new Promise((resolve, reject) => {
+		db.exec(query, param, (err) => {
+			if (err) {
+				reject(err);
+			} else {
+				resolve();
+			}
+		});
+	});
 }
