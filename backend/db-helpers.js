f5c8bc407bc995214fb39ed80dbb81bd6def1f26
Papzi <gamerboy53@live.dk>
2021-04-01 12:21:50 +0200

    fixed documentation

--- backend/db-helpers.js
+++ backend/db-helpers.js
@@ -5,7 +5,7 @@ const sqlite3 = require("sqlite3");
  * 
  * @param {sqlite3.Database} db the database to target
  * @param {string} query the sql query to execute
- * @param {array<string>} param the parameters to replace ? with
+ * @param {any[]} param the parameters to replace ? with
  * @returns {Promise<object[]>} the rows it found
  */
 exports.db_all = async function db_all(db, query, param) {
@@ -21,9 +21,10 @@ exports.db_all = async function db_all(db, query, param) {
 }
 
 /**
- * @param {*} db the database to target
- * @param {*} query the sql query to execute
- * @param {*} param the parameters to replace ? with
+ * 
+ * @param {sqlite3.Database} db the database to target
+ * @param {string} query the sql query to execute
+ * @param {any[]} param the parameters to replace ? with
  * @returns {Promise<object>} the row it found
  */
 exports.db_get = async function db_get(db, query, param) {
@@ -39,9 +40,10 @@ exports.db_get = async function db_get(db, query, param) {
 }
 
 /**
- * @param {*} db the database to target
- * @param {*} query the sql query to execute
- * @param {*} param the parameters to replace ? with
+ * 
+ * @param {sqlite3.Database} db the database to target
+ * @param {string} query the sql query to execute
+ * @param {any[]} param the parameters to replace ? with
  */
 exports.db_run = async function db_run(db, query, param) {
 	return await new Promise((resolve, reject) => {
