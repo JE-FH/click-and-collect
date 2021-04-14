ef4f4bf789d00d63d6d306ed5f4e01fe6e91a1d0
Papzi <gamerboy53@live.dk>
2021-04-14 14:29:50 +0200

    yeo

--- backend/timeslot-creator.js
+++ backend/timeslot-creator.js
@@ -1,6 +1,7 @@
 const sqlite3 = require("sqlite3");
 const moment = require("moment");
 const { dbGet, dbExec } = require("./db-helpers");
+const fs = require("fs/promises");
 
 async function main() {
 	db = new sqlite3.Database(__dirname + "/../databasen.sqlite3");
@@ -13,8 +14,15 @@ async function main() {
     await dbExec(db, databaseCreationCommand);
 
     console.log("Database correctly configured");
-
-	let last_timeslot = await dbGet(db, "select * from timeSlot ORDER BY endTime DESC LIMIT 1;");
+	let now = moment();
+	let lastTimeslot = await dbGet(db, "select * from timeSlot ORDER BY endTime DESC LIMIT 1;");
+	
+	let beginningTime = moment(lastTimeslot.startTime);
+	
+	if (now.isAfter(lastTimeslot)) {
+		beginningTime = now;
+	}
+	console.log(beginningTime);
 }
 
 main();
