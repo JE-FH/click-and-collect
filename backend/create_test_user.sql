f601e51640fcdec07ca43caa86989f8d1c39deb4
Karl Barnholdt Kragsaa <karlbarnholdt@gmail.com>
2021-04-16 14:36:13 +0200

    Fix duplicate order in demo webshop

--- backend/create_test_user.sql
+++ backend/create_test_user.sql
@@ -5,6 +5,10 @@ INSERT INTO store (id, name, openingTime, closingTime, pickupDelay, apiKey, stor
 INSERT INTO store (id, name, openingTime, closingTime, pickupDelay, apiKey, storeEmail) VALUES
 	(2, "Demo shop", "00:00:00", "00:00:00", "00:00:00", "demo-shop-94835577175941", "demo-shop@mail.com");
 
+--test user for "Demo shop" password is password
+INSERT INTO user (username, password, salt, name, superuser, storeId) VALUES
+	("marcManning", "e7620ce600f3434e87dc9bfdaacdcf473f98f1275838f74f92c7e928da4a76a24d134576898ec1143f9603b025850f9e269af92d7e068f31dec31bb07c97cebc", "abcdefg", "Marc Manning", 1, 2);
+
 --password is "password"
 INSERT INTO user (username, password, salt, name, superuser, storeId) VALUES
 	("bob", "e7620ce600f3434e87dc9bfdaacdcf473f98f1275838f74f92c7e928da4a76a24d134576898ec1143f9603b025850f9e269af92d7e068f31dec31bb07c97cebc", "abcdefg", "bob", 0, 4563);
