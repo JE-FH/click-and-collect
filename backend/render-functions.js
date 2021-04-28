5c34613abd585ae01a2c6a15ccdd7ffaf63ee412
Papzi <gamerboy53@live.dk>
2021-04-28 15:35:01 +0200

    bedre rækkefølge

--- backend/render-functions.js
+++ backend/render-functions.js
@@ -54,8 +54,8 @@ function renderEmployeeNav(store) {
 
 function generalHeader() {
     return `
-    <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
     `;
 }
 
